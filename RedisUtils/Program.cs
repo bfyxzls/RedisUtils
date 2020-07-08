790a11ba57b3c85b5836605b2e83c1d4145bd964
Lind <bfyxzls@sina.com>
2020-07-08 23:13:44 +0800

    init

---
+++ RedisUtils/Program.cs
@@ -0,0 +1,62 @@
+﻿using RedisUtils;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace redis.@lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisManager.Instance.GetDatabase(0).StringSet("hello", "你好");
+
+            wait();
+
+            Console.Read();
+        }
+
+        static void wait()
+        {
+            //如果锁被占用，第二个线程将等待
+            Task.Run(() =>
+            {
+                RedisLock.LockAwait("a1", "1", 10000, () =>
+                {
+                    Console.WriteLine("第1个执行");
+                    Thread.Sleep(10000);
+                });
+            });
+
+            Task.Run(() =>
+            {
+                RedisLock.LockAwait("a1", "1", 10000, () =>
+                {
+                    Console.WriteLine("第2个执行");
+                    Thread.Sleep(10000);
+                });
+            });
+        }
+        static void disposeMethod()
+        {
+            //如果锁被占用，第二个线程将不会被执行
+            Task.Run(() =>
+            {
+                RedisLock.Lock("a1", "1", 10000, () =>
+                {
+                    Console.WriteLine("第1个执行");
+                    Thread.Sleep(10000);
+                });
+            });
+
+            Task.Run(() =>
+            {
+                RedisLock.Lock("a1", "1", 10000, () =>
+                {
+                    Console.WriteLine("第2个执行");
+                    Thread.Sleep(10000);
+                });
+            });
+        }
+    }
+}
---
+++ RedisUtils/Redis/RedisLock.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace RedisUtils
+{
+    /// <summary>
+    /// redis分布锁
+    /// </summary>
+    public class RedisLock
+    {
+        /// <summary>
+        /// 加锁
+        /// </summary>
+        /// <param name="key">键，业务键</param>
+        /// <param name="value">对应的值 ，为一个guid码</param>
+        /// <param name="milliseconds">豪秒</param>
+        /// <param name="action">要执行的代码片断</param>
+        /// <returns></returns>
+        public static bool Lock(
+            string key,
+            string value,
+            int milliseconds,
+            Action action)
+        {
+
+            if (RedisManager.Instance.GetDatabase().LockTake(key, value, TimeSpan.FromMilliseconds(milliseconds)))
+            {
+                try
+                {
+                    Console.WriteLine("正在处理……");
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {   
+                    //处理结束后释放redis进程锁,否则还要阻塞100毫秒
+                    RedisManager.Instance.GetDatabase().LockRelease(key, value);
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("锁被占用……");
+                Thread.Sleep(100);
+                return false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// 锁等待
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="milliseconds"></param>
+        /// <param name="action"></param>
+        public static void LockAwait(
+           string key,
+           string value,
+           int milliseconds,
+           Action action)
+        {
+            while (true)
+            {
+                if (Lock(key, value, milliseconds, action))
+                {
+                    break;
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        }
+}
