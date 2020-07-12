d40c3697cdc313f149b927df919be26770229100
Lind <bfyxzls@sina.com>
2020-07-12 19:17:27 +0800

    添加redis使用说明

--- RedisUtils/Program.cs
+++ RedisUtils/Program.cs
@@ -1,5 +1,8 @@
-﻿using RedisUtils;
+﻿using Newtonsoft.Json;
+using RedisUtils;
+using StackExchange.Redis;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,11 +13,44 @@ namespace redis.@lock
         static void Main(string[] args)
         {
             RedisManager.Instance.GetDatabase(0).StringSet("hello", "你好");
+            redisList();
             //sql_insert("beijing", 20);
             //sql_insert("hebei", 25);
-            sql();
+            //sql();
 
             Console.Read();
+        }
+
+        static void redisList()
+        {
+            List<userinfo> list = new List<userinfo>();
+            list.Add(new userinfo { id = 1, name = "zzl" });
+            list.Add(new userinfo { id = 2, name = "lr" });
+            list.Add(new userinfo { id = 3, name = "bobo" });
+            list.Add(new userinfo { id = 4, name = "zzl2" });
+            list.Add(new userinfo { id = 5, name = "lr2" });
+            list.Add(new userinfo { id = 6, name = "bobo2" });
+            list.Add(new userinfo { id = 7, name = "zzl3" });
+            list.Add(new userinfo { id = 8, name = "lr3" });
+            list.Add(new userinfo { id = 9, name = "bobo3" });
+            list.Add(new userinfo { id = 10, name = "lr4" });
+            list.Add(new userinfo { id =11, name = "bobo4" });
+            foreach (userinfo u in list)
+            {
+                RedisManager.Instance.GetDatabase().SetAdd("hot_data", JsonConvert.SerializeObject(u));
+            }
+            IEnumerable<RedisValue> slist = RedisManager.Instance.GetDatabase().SetMembers("hot_data");
+            foreach (var s in slist)
+            {
+                Console.WriteLine(JsonConvert.DeserializeObject<userinfo>(s).ToString());
+            }
+
+            Console.WriteLine("hot_data is contain zzl:" + RedisManager.Instance.GetDatabase().SetContains("hot_data", JsonConvert.SerializeObject(new userinfo
+            {
+                id = 1,
+                name = "zzl"
+            })));
+
         }
         static void sql()
         {
--- RedisUtils/readme.md
+++ RedisUtils/readme.md
@@ -6,3 +6,16 @@
 3. redis3.x之后，推出redis cluster ，主要用来做数据的分片，它把请求打到16384多个槽上，然后进行存储。
 3主   3从的配置  它redis key的hash值进行计算得到槽位的
 比如：3台redis，它的key产生的hashcode，对3取模（余），得到的数一定是0~2之间数，对应我们redis的0到2号服务器
+
+4. StackExchange.Redis里的CommandFlags
+```
+None = 0;	//默认
+HighPriority = 1;	//不用了，废弃
+FireAndForget = 2; //对结果不感兴趣，调用者将会立即收到默认值
+PreferMaster = 0; //如果主服务器可用，则应在主服务器上执行此操作，但可以执行读操作
+DemandMaster = 4; //此操作只应在[主站]上执行
+PreferSlave = 8;	//如果可用，则应在[从站]上执行此操作，但将在其上执行
+DemandSlave = 12;	//此操作只应在[从站]上执行。 仅适用于读取操作。
+NoRedirect = 64;	//表示由于ASK或MOVED响应，不应将此操作转发到其他服务器
+NoScriptCache = 512 //表示与脚本相关的操作应使用EVAL，而不是SCRIPT LOAD + EVALSHA
+```
